2686b8a39412019738d80645bf0262de979b5ae1
andreasbloomquist <andreasbloomquist@gmail.com>
2015-06-15 14:30:38 -0700

    Seed data using Ffaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,20 @@
 require 'ffaker'
 
+(1..10).each do
+	user_params = {}
+	user_params[:email] = FFaker::Internet.email
+	user_params[:first_name] = FFaker::Name.first_name
+	user_params[:last_name] = FFaker::Name.last_name
+	user_params[:password] = "fakedata"
+	user_params[:password_confirmation] = user_params[:password]
+	user_params[:city] = FFaker::AddressUS.city
+	new_user = User.create(user_params)
+
+	(1..10).each do
+		new_log_post = LogPost.new
+		new_log_post.title = FFaker::HipsterIpsum.words(rand(8)+2).join(" ")
+		new_log_post.body = FFaker::HipsterIpsum.paragraphs(1 + rand(2)).join("\n")
+		new_log_post.save
+		new_user.log_posts << new_log_post
+	end
+end
