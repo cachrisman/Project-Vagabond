82d4ce8e40d12659b8e473521cbe01831178a8aa
Alex Kelley <alexkelley@Alexs-MacBook-Pro.local>
2015-06-16 12:30:19 -0700

    rspec log_post, and views/log_post/show.html.erb changes

--- spec/views/log_posts/show.html.erb_spec.rb
+++ spec/views/log_posts/show.html.erb_spec.rb
@@ -5,16 +5,29 @@ RSpec.describe "log_posts/show", type: :view do
     @log_post = assign(:log_post, LogPost.create!(
       :title => "Title",
       :body => "MyText",
-      :user => nil,
-      :city => nil
+      :user_id => 1,
+      :city_id => 1
     ))
+    @user = assign(:user, User.create!(
+                     :first_name => "Alex",
+                     :last_name => "Kelley",
+                     :city_id => 1,
+                     :email => "ak@me.com",
+                     :password => "nogood"
+                     ))
+
+    # log in the user
+    session[:user_id] = @user.id
+    current_user = @user
   end
 
-  it "renders attributes in <p>" do
+  it "renders all attributes in <p>" do
     render
-    expect(rendered).to match(/Title/)
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(//)
-    expect(rendered).to match(//)
+    expect(rendered).to match "Title"
+    expect(rendered).to match "MyText"
+    expect(rendered).to match @user.first_name
+    expect(rendered).to match @user.last_name
+    expect(rendered).to match "#{@log_post.city_id}"
   end
+
 end
