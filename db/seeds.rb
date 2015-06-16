70d6b18e341c0b259b6d4cb223d4f23578c8ed29
cachrisman <charlie.chrisman@gmail.com>
2015-06-16 14:26:06 -0700

    added continents to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ require 'ffaker'
 
 (1..10).each do
 
-	new_city = City.create({name: FFaker::Address.city})
+	new_city = City.create({name: FFaker::Address.city, continent: ["Africa", "Asia", "North America", "South America", "Europe", "Australia", "Antartica"].sample})
 
-	(1..10).each do
+	(1..5).each do
 		user_params = {}
 		user_params[:email] = FFaker::Internet.email
 		user_params[:first_name] = FFaker::Name.first_name
@@ -14,7 +14,7 @@ require 'ffaker'
 		user_params[:city_id] = new_city.id
 		new_user = User.create(user_params)
 
-		(1..10).each do
+		(1..5).each do
 			new_log_post = LogPost.new
 			new_log_post.title = FFaker::HipsterIpsum.words(rand(8)+2).join(" ")
 			new_log_post.body = FFaker::HipsterIpsum.paragraphs(1 + rand(2)).join("\n")
