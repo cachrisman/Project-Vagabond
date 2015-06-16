074b4d0cbc8e3a03fd2ab919db1af13d566a613e
andreasbloomquist <andreasbloomquist@gmail.com>
2015-06-16 16:20:12 -0700

    fix formatting

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -3,6 +3,7 @@ module CitiesHelper
 	def continents
 		@continents = City.all.select('continent').where.not(continent: nil).group('continent')
 	end
+	
 	def cities_in_continent(continent)
 		@continental_cities = City.all.where(continent: continent)
 	end
