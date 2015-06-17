0c9f365828dbb1ec5803b4629e9d4486f466d2e0
Alex Kelley <alexkelley@Alexs-MacBook-Pro.local>
2015-06-17 16:27:58 -0700

    refactored length of title validation

--- app/models/log_post.rb
+++ app/models/log_post.rb
@@ -1,12 +1,6 @@
 class LogPost < ActiveRecord::Base
  	validates :user_id, :city_id, :title, :body, presence: true
-        validates_length_of :title,
-                            :minimum => 1,
-                            :maximum => 200,
-                            :allow_blank => false
-        validates_length_of :body,
-                            :minimum => 1,
-                            :allow_blank => false
+        validates :title, length: { maximum: 200}
   
   belongs_to :user
   belongs_to :city
