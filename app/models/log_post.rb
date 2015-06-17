236bc1ffa43ca12153a64a90abac963a9087150f
Alex Kelley <alexkelley@Alexs-MacBook-Pro.local>
2015-06-17 15:38:08 -0700

    validate length of city, title for a new blog post

--- app/models/log_post.rb
+++ app/models/log_post.rb
@@ -1,5 +1,12 @@
 class LogPost < ActiveRecord::Base
  	validates :user_id, :city_id, :title, :body, presence: true
+        validates_length_of :title,
+                            :minimum => 1,
+                            :maximum => 200,
+                            :allow_blank => false
+        validates_length_of :body,
+                            :minimum => 1,
+                            :allow_blank => false
   
   belongs_to :user
   belongs_to :city
