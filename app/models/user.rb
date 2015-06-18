416182c29eb73f3a3045af85980c5ecbc654f2a0
Alex Kelley <alexkelley@Alexs-MacBook-Pro.local>
2015-06-18 11:14:43 -0700

    created following join table, modified User model accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, :email, :city_id, presence: true
   has_many :log_posts
   belongs_to :city
+  has_and_belongs_to_many :users,
+                          join_table: "following",
+                          association_foreign_key: "following_id"
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
